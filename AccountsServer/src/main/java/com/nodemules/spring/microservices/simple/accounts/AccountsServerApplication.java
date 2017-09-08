87fdf5c33560e2bb076eeefc83bdfc70838d833e
brenthaertlein <brenthaertlein@gmail.com>
2017-09-07 23:56:51 -0400

    more project structure nonsense

--- AccountsServer/src/main/java/com/nodemules/spring/microservices/simple/accounts/AccountsServerApplication.java
+++ AccountsServer/src/main/java/com/nodemules/spring/microservices/simple/accounts/AccountsServerApplication.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AccountsMicroserviceApplication {
+public class AccountsServerApplication {
   public static void main(String[] args) {
-    SpringApplication.run(AccountsMicroserviceApplication.class, args);
+    SpringApplication.run(AccountsServerApplication.class, args);
   }
 }
