ca07fd44797876632d412d54979c7bb2cb83d041
ProbableCohort <brenthaertlein@gmail.com>
2017-09-07 21:53:09 -0400

    initialize microservice application

---
+++ src/main/java/com/nodemules/spring/microservice/example/simple/Application.java
@@ -0,0 +1,17 @@
+package com.nodemules.spring.microservice.example.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author brent
+ * @since 9/7/17.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
